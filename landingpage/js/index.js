fe720854a784fc4dfaab9db8495810053e236b71
Spence Nelson <spenjmn@me.com>
2017-03-28 21:45:52 -0400

    added states

--- landingpage/js/index.js
+++ landingpage/js/index.js
@@ -1,3 +1,25 @@
 $( document ).ready(function() {
     $( ".branch" ).append('<div class="leaf"></div><div class="seed"></div><div class="flower"></div>');
+    
+
 });
+
+
+$( document ).ready(function() {
+    $('#explore-button').click(function(){
+    	$(this).addClass('active').siblings('.active').removeClass('active');
+    	$('.explore-view').addClass('active').siblings('.active').removeClass('active');
+	});
+
+	$('#play-button').click(function(){
+    	$(this).addClass('active').siblings('.active').removeClass('active');
+    	$('.play-view').addClass('active').siblings('.active').removeClass('active');
+	});
+
+	$('#think-button').click(function(){
+		$(this).addClass('active').siblings('.active').removeClass('active');
+    	$('.think-view').addClass('active').siblings('.active').removeClass('active');
+	});
+
+});
+
